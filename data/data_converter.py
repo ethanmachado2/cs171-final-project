93098c95415b59b5aaf28c8a2b062a8d76d80218
Sam Hubler <samhubler148@gmail.com>
2026-07-16 20:04:58 -0700

    converter for sqlite database file to make easily parseable csv files.

---
+++ data/data_converter.py
@@ -0,0 +1,26 @@
+import sqlite3  
+import csv
+
+database = 'database.sqlite' #database file to convert goes here
+
+conn = sqlite3.connect(database)
+
+cursor = conn.cursor()
+
+# Get all table names in DB
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+rows = cursor.fetchall()
+
+# Make CSV for each table for pandas when doing ML
+for row in rows:
+    print(row[0])
+    cursor.execute(f"SELECT * FROM {row[0]}")
+    rows = cursor.fetchall()
+    headers = [description[0] for description in cursor.description]
+    with open(f'{row[0]}.csv', 'w', newline='', encoding='utf8') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(rows)
+
+cursor.close()
+conn.close()
